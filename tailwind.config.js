4691bc6c5473ec67c96602f99c6b16c792df70f3
Jeevesh <jeevesh_rawat@epam.com>
2020-10-04 15:57:23 +0530

    feat(dark): add support for dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
   },
   theme: {
     extend: {
+      colors: {
+        'color-ts': '#007acc',
+        'color-js': '#f0db4f',
+        'color-swift': '#F05138',
+        'color-ng': '#c4473a',
+        'color-node': '#83CD29',
+        'color-react': '#61dafb'
+      },
       fontFamily: {
         'bangers': 'Bangers'
       },
